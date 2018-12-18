613688ebb67086f8166d4763c6286ac9012f346a
EyadA7med2 <43893562+EyadA7med2@users.noreply.github.com>
2018-12-18 20:33:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -211,6 +211,167 @@ client.on('message', msg => {
     }
 })
 
+   client.on('message', message => {
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "ban") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**انت لا تمتلك صلاحيه BAN_MEMBERS**");
+  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**انا لا امتلك صلاحيه MANAGE_MESSAGES**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+  /*let b5bzlog = client.channels.find("name", "5bz-log");
+  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
+  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+  if(!reason) return message.reply ("**اكتب سبب الطرد**");
+  if (!message.guild.member(user)
+  .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
+
+  message.guild.member(user).ban(7, user);
+
+  const banembed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setTitle('**Noobbot**')
+        .setDescription(`**Done Ban ${user}
+By ${message.author.username}** `)
+        .setAuthor(message.guild.name)
+  message.channel.send({
+    embed : banembed
+  })
+}
+});
+
+client.on('message', async rokz => {
+var prefix = "!"
+ 
+    if(rokz.content.startsWith(prefix + "Submit")) {
+ 
+      let lang = '';
+ 
+      let time = '';
+ 
+      let expe = '';
+ 
+      let fillter = m => m.author.id === rokz.author.id
+ 
+      await rokz.channel.send("**اشي الرتبة اللي تبيها ؟**").then(e => {
+ 
+     rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+ 
+     .then(co => {
+ 
+       lang = co.first().content;
+ 
+        co.first().delete();
+ 
+ 
+       e.edit(`ايش اسمك ؟
+[${lang}]`)
+ 
+       rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+ 
+       .then(col => {
+ 
+         time = col.first().content;
+ 
+          col.first().delete();
+ 
+ 
+            e.edit(`كم عمرك ؟
+[${time}]
+[${lang}]`)
+ 
+            rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+ 
+            .then(coll => {
+ 
+              expe = coll.first().content;
+ 
+               coll.first().delete();
+ 
+ 
+               e.edit(`جاري تقديمك...
+[${expe}]
+[${time}]
+[${lang}]`)
+ 
+              let rokzz = rokz.guild.channels.find("name","✵-「التقديمات")
+ 
+              setTimeout(() => {
+ 
+                e.edit("**تم التقديم**")
+ 
+              }, 3000)
+ 
+              rokzz.send(`
+» اللغة : **${lang}**
+» المدة : **${time}**
+» الخبرة : **${expe}**
+تم التقديم بواسطة: ${rokz.author}
+`).then(rokzzz => {
+ 
+                  rokzzz.react(":CheckMark:")
+ 
+                  rokzzz.react(":WrongMark:")
+ 
+                })
+ 
+            })
+ 
+       })
+ 
+     })
+ 
+   })
+ 
+    }
+ 
+  })
+
+client.on('message', message => {
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "kick") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**انت لا تمتلك صلاحيه KICK_MEMBERS**");
+  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**انا لا امتلك صلاحيه KICK_MEMBERS**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+  /*let b5bzlog = client.channels.find("name", "5bz-log");
+  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
+  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+  if(!reason) return message.reply ("**اكتب سبب الطرد**");
+  if (!message.guild.member(user)
+  .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
+
+  message.guild.member(user).kick(7, user);
+
+  const banembed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setTitle('**Noobbot**')
+        .setDescription(`**Done Kick ${user}
+By ${message.author.username}** `)
+        .setAuthor(message.guild.name)
+  message.channel.send({
+    embed : banembed
+  })
+}
+});
+
 
 
 
